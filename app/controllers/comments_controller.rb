332271346905c6b18e2553f40f04eae1484b17ca
LazyElegance13 <piyushthefallenangel@gmail.com>
2021-01-05 11:30:10 +0530

    first commit for official rails guide blog app

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+    include CommentsHelper
+
+    def create
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.create(comment_params)
+        redirect_to article_path(@article)
+    end
+end
